7c35a1238cc4bb239a90be93f07bc38fd425c578
nicolas <nicolas.braga@tarkuss.com.br>
2022-08-26 11:57:06 -0400

    Projeto concluido

--- src/paginas/Administracao/Pratos/FormularioPratos.tsx
+++ src/paginas/Administracao/Pratos/FormularioPratos.tsx
@@ -1,11 +1,4 @@
-import {
-  Box,
-  Button,
-  Container,
-  Paper,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import IPratos from "../../../interfaces/IPrato";
@@ -18,7 +11,10 @@ import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
+import user from "../../../assets/img/user.png";
+
 const FormularioPratos = () => {
+  /*  const [loading, setLoading] = useState(true); */
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
   const [tag, setTag] = useState("");
@@ -49,23 +45,35 @@ const FormularioPratos = () => {
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
-    if (parametros.id) {
-      api
-        .put(`pratos/${parametros.id}/`, {
-          nome: nome,
-        })
-        .then(() => {
-          alert("Prato atualizado");
-        });
-    } else {
-      api
-        .post("pratos/", {
-          nome: nome,
-        })
-        .then(() => {
-          alert("Prato cadastrado");
-        });
+
+    const formData = new FormData();
+
+    formData.append("nome", nome);
+    formData.append("descricao", descricao);
+    formData.append("tag", tag);
+    formData.append("restaurante", restaurante);
+
+    if (imagem) {
+      formData.append("imagem", imagem);
     }
+
+    api
+      .request({
+        url: "pratos/",
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        data: formData,
+      })
+      .then(() => {
+        setNome("")
+        setDescricao("")
+        setRestaurante("")
+        setTag("")
+        alert("Prato cadastrado com sucesso!")
+      })
+      .catch((error) => console.log(error));
   };
 
   const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
@@ -77,83 +85,115 @@ const FormularioPratos = () => {
   };
 
   return (
-    <Box>
-      <Container maxWidth="lg" sx={{ mt: 1 }}>
-        <Paper sx={{ p: 2 }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              flexGrow: 1,
-            }}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        flexGrow: 1,
+      }}
+    >
+      <Typography component="h1" variant="h6">
+        Formulário de Pratos
+      </Typography>
+      <Box sx={{ width: "100%" }} component="form" onSubmit={aoSubmeterForm}>
+        <TextField
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          label="Nome"
+          variant="standard"
+          fullWidth
+          required
+          margin="dense"
+        />
+        <TextField
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          label="Descricao"
+          variant="standard"
+          fullWidth
+          required
+          margin="dense"
+        />
+
+        <FormControl margin="dense" fullWidth>
+          <InputLabel id="select-tag">Tipo</InputLabel>
+          <Select
+            labelId="select-tag"
+            value={tag}
+            label="Tipo"
+            onChange={(e) => setTag(e.target.value)}
           >
-            <Typography component="h1" variant="h6">
-              Formulário de Pratos
-            </Typography>
-            <Box component="form" onSubmit={aoSubmeterForm}>
-              <TextField
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-                label="Nome"
-                variant="standard"
-                fullWidth
-                required
-                margin="dense"
-              />
-              <TextField
-                value={descricao}
-                onChange={(e) => setDescricao(e.target.value)}
-                label="Descricao"
-                variant="standard"
-                fullWidth
-                required
-                margin="dense"
-              />
+            {tags.map((tag) => (
+              <MenuItem key={tag.id} value={tag.value}>
+                {tag.value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-              <FormControl margin="dense" fullWidth>
-                <InputLabel id="select-tag">Tipo</InputLabel>
-                <Select
-                  labelId="select-tag"
-                  id="demo-simple-select"
-                  value={tag}
-                  label="Tipo"
-                  onChange={(e) => setTag(e.target.value)}
+        <FormControl margin="dense" fullWidth>
+          <InputLabel id="select-tag-res">Restaurante</InputLabel>
+          <Select
+            labelId="select-tag-res"
+            value={restaurante}
+            label="Restaurante"
+            onChange={(e) => setRestaurante(e.target.value)}
+          >
+            {restaurantes.map((restaurante) => (
+              <MenuItem key={restaurante.id} value={restaurante.id}>
+                {restaurante.nome}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <Stack alignItems="center" spacing={3}>
+          {imagem ? (
+            <img
+              style={{ height: "300px", borderRadius: "50%" }}
+              src={URL.createObjectURL(imagem)}
+              alt="Foto Prato"
+            />
+          ) : (
+            <img
+              style={{ height: "300px", borderRadius: "50%" }}
+              src={user}
+              alt="Foto Prato"
+            />
+          )}
+
+          <Button fullWidth variant="contained" component="label">
+            Buscar
+            <input hidden type="file" onChange={selecionarArquivo} />
+          </Button>
+        </Stack>
+        <Button sx={{ mt: 2 }} fullWidth type="submit" variant="outlined">
+          Salvar
+        </Button>
+
+        {/*  {loading ? (
+                <Button
+                  sx={{ mt: 2 }}
+                  fullWidth
+                  type="submit"
+                  variant="outlined"
                 >
-                  {tags.map((tag) => (
-                    <MenuItem key={tag.id} value={tag.id}>
-                      {tag.value}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-
-              <FormControl margin="dense" fullWidth>
-                <InputLabel id="select-tag">Tipo</InputLabel>
-                <Select
-                  labelId="select-tag"
-                  id="demo-simple-select"
-                  value={restaurante}
-                  label="Restaurante"
-                  onChange={(e) => setRestaurante(e.target.value)}
+                  Salvar
+                </Button>
+              ) : (
+                <LoadingButton
+                  sx={{ mt: 2 }}
+                  loading
+                  loadingPosition="start"
+                  startIcon={<FaSave />}
+                  variant="outlined"
+                  fullWidth
                 >
-                  {restaurantes.map((restaurante) => (
-                    <MenuItem key={restaurante.id} value={restaurante.id}>
-                      {restaurante.nome}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-
-              <input type="file" onChange={selecionarArquivo} />
-
-              <Button sx={{ mt: 2 }} fullWidth type="submit" variant="outlined">
-                Salvar
-              </Button>
-            </Box>
-          </Box>
-        </Paper>
-      </Container>
+                  Salvando...
+                </LoadingButton>
+              )} */}
+      </Box>
     </Box>
   );
 };
